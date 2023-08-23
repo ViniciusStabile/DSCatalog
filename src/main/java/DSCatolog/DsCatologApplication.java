e69943d2a9f2784851fdbe80e2870a713ef35819
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-08-23 16:11:17 -0300

    First commit

---
+++ src/main/java/DSCatolog/DsCatologApplication.java
@@ -0,0 +1,13 @@
+package DSCatolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCatologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCatologApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DSCatolog/DsCatologApplicationTests.java
@@ -0,0 +1,13 @@
+package DSCatolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatologApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
