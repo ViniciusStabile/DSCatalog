685c60a80cf9ebe450d629b738820ca8b7427034
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-08-24 22:05:14 -0300

    DTO

--- src/main/java/DSCatolog/services/CategoryService.java
+++ src/main/java/DSCatolog/services/CategoryService.java
@@ -1,12 +1,16 @@
 package DSCatolog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import DSCatolog.DTO.CategoryDTO;
 import DSCatolog.entities.Category;
 import DSCatolog.repositories.CategoryRepository;
+
 
 @Service
 public class CategoryService {
@@ -14,9 +18,10 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll(){
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll(){
 		List<Category> list = repository.findAll();
-		return list;
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		
 	}
 	
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_category(name) VALUES ('BOOKS');
+INSERT INTO tb_category(name) VALUES ('COMPUTERS');
+INSERT INTO tb_category(name) VALUES ('ELECTRONICS');
