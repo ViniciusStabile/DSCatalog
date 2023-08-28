fe822a7fc8e49423009537823d527750a51d414b
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-08-28 16:56:00 -0300

    ProductDTO

---
+++ src/main/java/DSCatolog/DTO/ProductDTO.java
@@ -0,0 +1,108 @@
+package DSCatolog.DTO;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import DSCatolog.entities.Category;
+import DSCatolog.entities.Product;
+
+public class ProductDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	private Instant date;
+
+	private List<CategoryDTO> list = new ArrayList<>();
+
+	public ProductDTO() {
+
+	}
+
+	public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+		this.date = date;
+	}
+
+	public ProductDTO(Product entity) {
+		id = entity.getId();
+		name = entity.getName();
+		description = entity.getDescription();
+		price = entity.getPrice();
+		imgUrl = entity.getImgUrl();
+		date = entity.getDate();
+	}
+
+	public ProductDTO(Product entity, Set<Category> categories) {
+		this(entity);
+		for (Category list : categories) {
+			CategoryDTO cat = new CategoryDTO(list);
+			this.list.add(cat);
+		}
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Instant getdate() {
+		return date;
+	}
+
+	public void setdate(Instant date) {
+		this.date = date;
+	}
+
+	public List<CategoryDTO> getList() {
+		return list;
+	}
+
+}
